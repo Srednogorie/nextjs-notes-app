b214aa94cc7d03e49b3f085c966234dcf34deddb
Leno <akrachunov@gmail.com>
2021-04-21 20:35:54 +0100

    Cover styling & theme-ui.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import React from 'react'
 import Link from 'next/link'
+import '../styles/Home.module.css'
+
 
 const Page = () => (
-    <div>
-        <h1>Index page</h1>
-
-        <Link href="/notes">
-            <a>Notes</a>
-        </Link>
+    <div sx={{ height: `calc(100vh - 60px)`}}>
+        <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
+            <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+        </div>
     </div>
 )
-
 export default Page
