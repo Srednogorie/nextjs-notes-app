8c793939c1f79784f6249bcac052dfac313420e3
Leno <akrachunov@gmail.com>
2021-04-24 19:26:22 +0100

    Install corse & next-connect.

---
+++ pages/api/note/index.js
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// pages/api/data
+import nc from 'next-connect';
+import cors from 'cors';
+
+const handler = nc()
+    // use connect based middleware
+    .use(cors())
+    // express like routing for methods
+    .get((req, res) => {
+      res.send('Hello world')
+    })
+    .post((req, res) => {
+      res.json({ hello: 'world' })
+    })
+    .put(async (req, res) => {
+      res.end('hello')
+    })
+
+export default handler;
